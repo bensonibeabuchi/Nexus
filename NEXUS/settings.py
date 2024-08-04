dc835232f8714cf5a9b91204461d55abb7e80e7d
bensonibeabuchi <ibeabuchibenson@gmail.com>
2024-08-04 17:25:31 +0100

    fixed git guardian warning

--- NEXUS/settings.py
+++ NEXUS/settings.py
@@ -101,9 +101,9 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'railway',
         'USER': 'postgres',
-        'PASSWORD': 'MkyqZFIsxfmAvrNekIuYfjyvQNXGcHMp',
-        'HOST': 'monorail.proxy.rlwy.net',
-        'PORT': '59772',
+        'PASSWORD': config('RAILWAY_PASSWORD'),
+        'HOST': config('RAILWAY_HOST'),
+        'PORT': config('RAILWAY_PORT'),
     }
 }
 
@@ -143,8 +143,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'bensonibeabuchistudios@gmail.com'
-EMAIL_HOST_PASSWORD = 'xyqogzvptsxkykpf'
+EMAIL_HOST_USER = config('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
 
 
 # Static files (CSS, JavaScript, Images)
